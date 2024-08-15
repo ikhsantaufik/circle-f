44636418f7b9ba9cf3647a34966365fd357ebcb4
ikhsantaufik <ikhsanp34@gmail.com>
2024-08-15 17:06:01 +0700

    tetre

--- src/configs/vite-env.d.ts
+++ src/configs/vite-env.d.ts
@@ -1,7 +1,7 @@
 interface ImportMetaEnv {
     readonly VITE_BASE_URL: string
-    // readonly VITE_OPENAI_BASE_URL: string
-    // readonly VITE_OPENAI_API_KEY: string
+    readonly VITE_OPENAI_BASE_URL: string
+    readonly VITE_OPENAI_API_KEY: string
 }
 
 interface ImportMeta {
